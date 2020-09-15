6b0db9596e23fa3335a5619d8091101fcac00d33
Crowdbotics <team@crowdbotics.com>
2020-09-15 01:18:22 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/why_math_20274/MainActivity.java
@@ -0,0 +1,15 @@
+package com.why_math_20274;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "why_math_20274";
+    }
+}
